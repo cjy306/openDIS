612c61e762c39760be4e337ec48bc6cff231e919
CJY306 <1905435286@qq.com>
2026-04-23 10:27:31 +0800

    1

--- HomeWork/submit.sh
+++ HomeWork/submit.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1           # Request 1 node
 #SBATCH --ntasks=1          # Total number of tasks
 #SBATCH --cpus-per-task=48  # Number of CPU cores per task
-#SBATCH --time 48:00:00
+#SBATCH --time 6:00:00
 export OMP_NUM_THREADS=48
 export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
@@ -13,4 +13,4 @@ export OMP_PLACES=threads
 module purge
 module load miniforge/25.3.1
 source activate opendis_cpu
-python //data/home/dg000246b/openDIS/HomeWork/test_Cu.py --restart 2400
+python /data/home/dg000246b/openDIS/HomeWork/test_Cu_twin.py
