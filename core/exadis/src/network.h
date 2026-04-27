9873a7f70e55401e416095c7f7dde8a3f8d935be
cjy306 <cjy306@example.com>
2026-04-27 09:28:39 +0800

    debug

--- core/exadis/src/network.h
+++ core/exadis/src/network.h
@@ -143,7 +143,7 @@ struct DisSeg
  *    Struct:        Conn
  *
  *-------------------------------------------------------------------------*/
-#define MAX_CONN 10
+#define MAX_CONN 20
 struct Conn 
 {
     int num;
