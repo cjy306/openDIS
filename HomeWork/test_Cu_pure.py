6eb725d72cad117cc39e8d27fdd717585ad61cfa
CJY306 <1905435286@qq.com>
2026-05-06 10:50:08 +0800

    update

--- HomeWork/test_Cu_pure.py
+++ HomeWork/test_Cu_pure.py
@@ -26,8 +26,8 @@ state = {
     "minseg":    400,
     "rtol":      0.25,
     "rann":      2.0,
-    "nextdt":    1e-9,
-    "maxdt":     1e-6,
+    "nextdt":    1e-12,
+    "maxdt":     1e-7,
 }
 
 
@@ -54,7 +54,7 @@ def main():
         restart = None
 
     calforce  = CalForce(force_mode='SUBCYCLING_MODEL', state=state, Ngrid=64, cell=net.get_disnet(ExaDisNet).cell)
-    mobility  = MobilityLaw(mobility_law='FCC_0', state=state, Medge=64103.0, Mscrew=64103.0, vmax=20000.0)
+    mobility  = MobilityLaw(mobility_law='FCC_0', state=state, Medge=64103.0, Mscrew=64103.0, vmax=50.0)
     timeint   = TimeIntegration(integrator='Subcycling', rgroups=[0.0, 300.0, 1000.0, 2000.0], state=state, force=calforce, mobility=mobility)
     collision = Collision(collision_mode='Retroactive', state=state)
     topology  = Topology(topology_mode='TopologyParallel', state=state, force=calforce, mobility=mobility)
@@ -70,7 +70,7 @@ def main():
         burgmag=state["burgmag"],
         state=state,
         print_freq=1,
-        write_freq=100,
+        write_freq=50,
         write_dir=output_dir,
         restart=restart,
     )
