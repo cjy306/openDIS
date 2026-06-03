97d524652c1329c829ec8078ed334c2c30eb9983
CJY306 <1905435286@qq.com>
2026-06-03 15:36:51 +0800

    update

--- core/exadis/src/network.h
+++ core/exadis/src/network.h
@@ -143,7 +143,7 @@ struct DisSeg
  *    Struct:        Conn
  *
  *-------------------------------------------------------------------------*/
-#define MAX_CONN 10
+#define MAX_CONN 15
 struct Conn 
 {
     int num;
