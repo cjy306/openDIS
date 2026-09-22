b202ce3e5b841c6e4714ed3ec7b09e3831485ba1
cjy306 <1905435286@qq.com>
2026-09-22 09:47:04 +0800

    update

--- cr_coherency_test/test_with_coherency.py
+++ cr_coherency_test/test_with_coherency.py
@@ -112,7 +112,7 @@ def main() -> None:
             cross_slip=None,
             loading_mode="stress",
             applied_stress=np.zeros(6),
-            max_step=50000,
+            max_step=5000,
             burgmag=state["burgmag"],
             state=state,
             print_freq=1,
